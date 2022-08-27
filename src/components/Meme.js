22f1eef710ad2e3b933e79a1d8190e989e0abd77
Ali Fahed <alifahed67@gmail.com>
2022-08-27 14:32:52 +0800

    Refactor State

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -2,13 +2,23 @@ import React from 'react';
 import memesData from '../memesData.js';
 
 function Meme() {
-  const [memeImage, setMemeImage] = React.useState('');
+  const [meme, setMeme] = React.useState({
+    topText: '',
+    bottomText: '',
+    randomImage: '',
+  });
+
+  const [allMemeImages, setAllMemeImages] = React.useState(memesData);
 
   function getMemeImage() {
-    const memesArray = memesData.data.memes;
+    const memesArray = allMemeImages.data.memes;
     const randomMeme = Math.floor(Math.random() * memesArray.length);
     const url = memesArray[randomMeme].url;
-    setMemeImage(url);
+
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      randomImage: url,
+    }));
 
     const memeImageContainer = document.querySelector('.meme-image-container');
     memeImageContainer.classList.add('display-meme-image');
@@ -39,7 +49,11 @@ function Meme() {
       {/* </form> */}
 
       <div className="meme-image-container">
-        <img src={memeImage} alt="Random Meme Image" className="meme-image" />
+        <img
+          src={meme.randomImage}
+          alt="Random Meme Image"
+          className="meme-image"
+        />
       </div>
     </main>
   );
